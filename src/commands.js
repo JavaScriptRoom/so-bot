883122cd610845102431d08db5336d2094d85329
Ben <ben@prestigegifting.co.uk>
2017-08-23 14:25:35 +0100

    Added basic MDN command.

--- src/commands.js
+++ src/commands.js
@@ -1,8 +1,20 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
 module.exports = {
     echo({ args, event }, reply) {
         reply(args.join(' '));
     },
     countArgs({ args, event }, reply) {
         reply(args.map((arg, i) => `${i}:${arg}`).join(' '));
+    },
+    async mdn({ args, event }, reply) {
+        const body = await request(`https://developer.mozilla.org/en-US/search?q=${args.join(' ')}`);
+        const $ = cheerio.load(body);
+        const results = $('.result-list .result-list-item h4 a').map((i, link) => {
+            return `[${$(link).text()}](${$(link).attr('href')})`;
+        }).get();
+        const result = `:${event.message_id} MDN: ${results.slice(0, 5).join(', ')}`;
+        reply(result);
     }
 };
